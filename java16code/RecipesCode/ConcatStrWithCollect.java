9ec04c2239bb1cdbbf05a665328d947f32bf2969
Julian Garcia Ortiz <jugarcia01@hotmail.com>
2023-02-10 00:55:54 -0500

    Corregidos errores en ConcatStrWithCollect.java

--- java16code/RecipesCode/ConcatStrWithCollect.java
+++ java16code/RecipesCode/ConcatStrWithCollect.java
@@ -8,17 +8,17 @@ class ConcatStrWithCollect {
   public static void main(String... args) {
     Stream<String> streamWords = Stream.of("this", "is", "a", "list", "of", "strings");
 
-    var result1 = concatUsingLambdas(Stream inputWords);
+    var result1 = concatUsingLambdas(streamWords);
 
   }
 
-  static String concatUsingLambdas(Stream inWords) {
+  static String concatUsingLambdas(Stream<String> inWords) {
     String resultStr = inWords
       .collect(() -> new StringBuilder(),
-          (sb, str) -> sb.append(str),
+          (sb, str) -> sb.append(str + ".."),
           (sb1, sb2) -> sb1.append(sb2))
       .toString();
-      
+    System.out.println(resultStr);
     return resultStr;
   }
 
