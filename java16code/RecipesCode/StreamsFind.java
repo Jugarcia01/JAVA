f0e83f32f2a70e56928ab034309005af8bcef05d
Julian Garcia Ortiz <jugarcia01@hotmail.com>
2023-08-25 00:38:12 -0500

    Adicionado clase  StreamsFind

--- java16code/RecipesCode/StreamsFind.java
+++ java16code/RecipesCode/StreamsFind.java
@@ -7,7 +7,9 @@ class StreamsFind {
 
   public static void main (String[] args) {
  // Finding the first even integer
- Optional<Integer> firstEven = Stream.of(3, 1, 4, 1, 5, 7, 8, 6, 9, 2, 5).filter(n -> n % 2 == 0).findFirst();
+ Optional<Integer> firstEven = Stream.of(3, 1, 4, 1, 5, 7, 8, 6, 9, 2, 5)
+   .filter(n -> n % 2 == 0)
+   .findFirst();
 
  System.out.println("First even: " + firstEven);
 
